b0a69c0b6464a1e89cc750809a0730a18d4545f0
Kitin <heykitin@outlook.com>
2024-04-08 09:18:44 +0800

    Update lnmp.sh

--- web/lnmp.sh
+++ web/lnmp.sh
@@ -96,7 +96,7 @@ wget -t0 -c https://www.openssl.org/source/old/1.1.1/openssl-${openssl1_ver}.tar
 wget -t0 -c https https://boostorg.jfrog.io/artifactory/main/release/${boost_ver}/source/boost_${boost_ver2}.tar.gz
 
 ## jemalloc
-if [ "${dbinstallmethod}" == "1" ]; then
+if [ "$(get_ip_country)" == "CN" ]; then
 	wget -t0 -c https://gh.kitin.cc/https://github.com/jemalloc/jemalloc/archive/${jemalloc_ver}.tar.gz
 else
 	wget -t0 -c https://github.com/jemalloc/jemalloc/archive/${jemalloc_ver}.tar.gz
